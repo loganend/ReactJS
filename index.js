6acb8e3da94108e7e26fe8daa35b0ef9d0e0e470
sergey90x <cheremisin.sergey@yandex.ru>
2017-12-01 18:28:47 +0300

    redux_async done

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
 import { applyMiddleware, createStore } from "redux";
+import axios from "axios";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
-const reducer = (initialState = 0, action) => {
-	if(action.type === "INC"){
-		return initialState + 1;
-	}else if(action.type ==="DEC"){
-		return initialState - 1;
-	}else if(action.type === "E"){
-		throw new Error("AAA");
+const initState = {
+	fetching: false,
+	fetched: false,
+	users: [],
+	error: null,
+}
+
+const reducer = (state=initState, action) => {
+	switch(action.type){
+		case "FETCH_USERS_PENDING": {
+			return {...state, fetching: true}
+			break;
+		}
+		case "FETCH_USERS_REJECTED": {
+			return {...state, fetching: false, error: action.payload}
+			break;
+		}
+		case "RECIEVE_USER_FULLFILLED": {
+			return {...state, fetching: false, fetched: true, users: action.payload}
+			break;
+		}
+		return state;
 	}
-	return initialState;
+
+	return state;
 }
 
-const logger = (store) => (next) => (action) => {
-	console.log("action fired", action);
-	// action.type = "DEC";
-	next(action);
-}
+const middleware = applyMiddleware(promise(), thunk, logger)
+const store = createStore(reducer, middleware)
 
-const error = (store) => (next) => (action) => {
-	try {
-		next(action);
-	}catch(e){
-		console.log("Error", e);
-	}
-}
+// store.subscribe(() => {
+// 	console.log("store changed", store.getState());
+// })
 
-const middleware = applyMiddleware(logger, error);
-
-const store = createStore(reducer, 1, middleware);
-
-
-store.subscribe(() => {
-	console.log("store changed", store.getState());
+store.dispatch({
+	type: "FETCH_USERS",
+	payload: axios.get("http://rest.learncode.academy/api/wstern/users")
 })
 
 
-store.dispatch({type: "INC"});
-
-store.dispatch({type: "INC"});
-
-store.dispatch({type: "DEC"});
-
-store.dispatch({type: "DEC"});
-
-store.dispatch({type: "DEC"});
-
-store.dispatch({type: "E"});
+// store.dispatch((dispatch) => {
+// 	dispatch({type: "FETCH_USERS_START"})
+// 	axios.get("http://rest.learncode.academy/api/wstern/users")
+// 		.then((response) => {
+// 			dispatch({type: "RECIEVE_USER", payload: response.data})
+// 		})
+// 		.catch((err) => {
+// 			dispatch({type: "FETCH_USERS_ERROR", payload: err})
+// 		})
+// });
